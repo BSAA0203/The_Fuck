b3a758a0ebb2484d59dd8f28beb38e14fc3a8ba5
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2020-05-01 19:19:37 +0900

    뉴스 크롤링 트리거

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,54 @@
 const functions = require('firebase-functions');
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
+const axios = require('axios');
 
-exports.tick_tock = functions
+const op = {
+    timeoutSeconds: 60,
+    memory: '512MB'
+}
+
+exports.news = functions
+    .runWith(op)
+    .region('asia-northeast1')
+    .https
+    .onRequest(() => {
+        const getData = async () => {
+            try {
+                const browser = await puppeteer.launch({
+                    args: ['--no-sandbox', '--disable-setuid-sandbox']
+                });
+                const page = await browser.newPage();
+                await page.goto('https://news.naver.com/main/ranking/popularDay.nhn');
+                const news = await page.evaluate(() => {
+                    return document
+                        .querySelector("#right_dailyList")
+                        .textContent
+                        .replace(/\t|\s+$/g, '');
+                });
+                console.log(news);
+                await browser.close();
+                console.log('success');
+            } catch (error) {
+                console.log('wtf : ', error);
+            }
+        };
+        getData();
+        return null;
+    })
+
+exports.ticktock = functions
+    .runWith(op)
     .region('asia-northeast1')
     .pubsub
     .schedule('0 0 * * *')
     .timeZone('Asia/Seoul')
-    .onRun(() => {
-    })
+    .onRun(async() => {
+        try {
+            return await axios.get(
+                'https://asia-northeast1-the-fuck-a25fd.cloudfunctions.net/news'
+            );
+        } catch (error) {
+            console.log(error);
+        }
+    });
