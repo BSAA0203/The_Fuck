38515989a781c9e6f8430b7c364c89b3840c9c39
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2020-05-02 18:33:03 +0900

    매크로 편지 완성
    낮 12시 크롤링 시도 & 위문편지 자동 전송
    
    저녁 6시전까진 받을 수 있기를

--- functions/index.js
+++ functions/index.js
@@ -20,13 +20,66 @@ exports.news = functions
                 });
                 const page = await browser.newPage();
                 await page.goto('https://news.naver.com/main/ranking/popularDay.nhn');
+                const title = '오늘의 뉴우스';
                 const news = await page.evaluate(() => {
                     return document
                         .querySelector("#right_dailyList")
-                        .textContent
-                        .replace(/\t|\s+$/g, '');
+                        .textContent;
                 });
                 console.log(news);
+
+                const ID = "smw0393@naver.com";
+                const PW = "Apsj1860178*";
+                await page.goto('https://www.thecamp.or.kr/login/viewLogin.do');
+                await page.evaluate((id, pw) => {
+                    document
+                        .querySelector('#userId')
+                        .value = id;
+                    document
+                        .querySelector('#userPwd')
+                        .value = pw;
+                }, ID, PW);
+                await page.click('#emailLoginBtn');
+                await page.waitForNavigation();
+
+                await page.goto('https://www.thecamp.or.kr/eduUnitCafe/viewEduUnitCafeMain.do')
+                await page.waitForSelector(
+                    '#divSlide1 > div.swiper-wrapper > div.swiper-slide.swiper-slide-active.swiper-' +
+                    'slide-duplicate-next.swiper-slide-duplicate-prev > div > div.btn-wrap > a.btn-' +
+                    'green'
+                );
+                await page.click(
+                    '#divSlide1 > div.swiper-wrapper > div.swiper-slide.swiper-slide-active.swiper-' +
+                    'slide-duplicate-next.swiper-slide-duplicate-prev > div > div.btn-wrap > a.btn-' +
+                    'green'
+                );
+                await page.waitForSelector(
+                    'body > div.container > div.container-wrap > div:nth-child(2) > div.btn-a-wrap.' +
+                    'text-center.mt50 > button'
+                );
+                await page.click(
+                    'body > div.container > div.container-wrap > div:nth-child(2) > div.btn-a-wrap.' +
+                    'text-center.mt50 > button'
+                );
+
+                await page.waitForSelector('#cke_1_contents > iframe');
+                console.log('\niframe is ready. Loading iframe content');
+                await page.evaluate((tt) => {
+                    document
+                        .querySelector('#sympathyLetterSubject')
+                        .value = tt;
+                }, title);
+                const el = await page.$('#cke_1_contents > iframe');
+                const frame = await el.contentFrame();
+                await frame.evaluate((info) => {
+                    document
+                        .querySelector('body > p')
+                        .innerText = info;
+                }, news)
+                await page.click(
+                    'body > div.container > div.container-wrap > section > div.btn-b-area > a:nth-c' +
+                    'hild(3)'
+                );
                 await browser.close();
                 console.log('success');
             } catch (error) {
@@ -41,9 +94,9 @@ exports.ticktock = functions
     .runWith(op)
     .region('asia-northeast1')
     .pubsub
-    .schedule('0 0 * * *')
+    .schedule('0 12 * * *')
     .timeZone('Asia/Seoul')
-    .onRun(async() => {
+    .onRun(async () => {
         try {
             return await axios.get(
                 'https://asia-northeast1-the-fuck-a25fd.cloudfunctions.net/news'
