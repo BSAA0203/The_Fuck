845212bcc6c7dbf5d2046417a97d86dad58da2a9
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2020-05-08 01:56:55 +0900

    일본 뉴스 편지 수정

--- functions/index.js
+++ functions/index.js
@@ -104,6 +104,96 @@ exports.jp_news = functions
     .region('asia-northeast1')
     .https
     .onRequest(async () => {
+        try {
+            const browser = await puppeteer.launch({
+                args: ['--no-sandbox', '--disable-setuid-sandbox']
+            });
+            const page = await browser.newPage();
+            await page.setDefaultNavigationTimeout(0);
+            await page.goto('https://www3.nhk.or.jp/nhkworld/ko/news/list/');
+            await page.waitForSelector(
+                'body > div > div > div > div > div > div:nth-child(2)'
+            );
+            const title = '오늘의 일본 뉴우스';
+            const news = await page.evaluate(() => {
+                return document
+                    .querySelector(
+                        "body > div > div > div > div > div > div:nth-child(2)"
+                    )
+                    .outerText;
+            });
+            console.log(news);
+
+            const ID = "smw0393@naver.com";
+            const PW = "Apsj1860178*";
+            await page.goto(
+                'https://www.thecamp.or.kr/login/viewLogin.do',
+                {waitUntil: "domcontentloaded"}
+            );
+            await page.evaluate((id, pw) => {
+                document
+                    .querySelector('#userId')
+                    .value = id;
+                document
+                    .querySelector('#userPwd')
+                    .value = pw;
+                document
+                    .querySelector('#emailLoginBtn')
+                    .click();
+            }, ID, PW);
+            await page.waitForNavigation();
+            console.log('login success');
+
+            await page.goto(
+                'https://www.thecamp.or.kr/eduUnitCafe/viewEduUnitCafeMain.do',
+                {waitUntil: "domcontentloaded"}
+            );
+            await page.evaluate(() => {
+                document
+                    .querySelector(
+                        '#divSlide1 > div.swiper-wrapper > div.swiper-slide.swiper-slide-active.swiper-' +
+                        'slide-duplicate-next.swiper-slide-duplicate-prev > div > div.btn-wrap > a.btn-' +
+                        'green'
+                    )
+                    .click();
+            });
+            await page.waitForNavigation();
+            await page.evaluate(() => {
+                document
+                    .querySelector(
+                        'body > div.container > div.container-wrap > div:nth-child(2) > div.btn-a-wrap.' +
+                        'text-center.mt50 > button'
+                    )
+                    .click();
+            });
+            await page.waitForNavigation();
+            await page.waitForSelector('#cke_1_contents > iframe');
+            console.log('\niframe is ready. Loading iframe content');
+            await page.evaluate((tt) => {
+                document
+                    .querySelector('#sympathyLetterSubject')
+                    .value = tt;
+            }, title);
+            const el = await page.$('#cke_1_contents > iframe');
+            const frame = await el.contentFrame();
+            await frame.evaluate((info) => {
+                document
+                    .querySelector('body > p')
+                    .innerText = info;
+            }, news);
+            await page.evaluate(() => {
+                document
+                    .querySelector(
+                        'body > div.container > div.container-wrap > section > div.btn-b-area > a:nth-c' +
+                        'hild(3)'
+                    )
+                    .click();
+            })
+            await browser.close();
+            console.log('success');
+        } catch (error) {
+            console.log('WTF : ', error);
+        }
         return null;
     });
 
